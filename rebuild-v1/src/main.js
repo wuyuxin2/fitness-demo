a51a7b848cfa5f0aa8cdc172603a5736357b60c3
wuyuxin2 <wuyuxin_0518@163.com>
2025-02-07 16:08:07 +0800

    删除重构代码

--- rebuild-v1/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './App.vue';
-import './assets/main.css'; // 引入样式
-
-const app = createApp(App);
-app.use(createPinia());
-app.mount('#app');
